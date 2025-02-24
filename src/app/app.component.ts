66fbbb5ab277ac78ee2965a534edee8cb96888e3
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-24 13:27:34 -0300

    feat: integrate About component into main app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { AboutComponent } from './components/about/about.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent, ProjectsComponent],
+  imports: [HeaderComponent, HomeComponent, AboutComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
