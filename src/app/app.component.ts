2634d29a1c8d4140c7c7468b12c242e2616671b3
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-24 12:28:58 -0300

    feat: integrate Projects component into main app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ProjectsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
