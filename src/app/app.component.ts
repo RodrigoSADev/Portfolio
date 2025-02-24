dfc7f729975ec3317780ea000e47ca511873d4de
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-24 13:13:19 -0300

    feat: add Header component with navigation and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HomeComponent, ProjectsComponent],
+  imports: [HeaderComponent, HomeComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
