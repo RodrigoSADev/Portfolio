319d66bf7aee15fa67784f02d1b5f8d032e01945
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-24 13:27:06 -0300

    feat: add Experiences component with work history and logos

---
+++ src/app/components/experiences/experiences.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiences',
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.scss',
+})
+export class ExperiencesComponent {
+  metasixStack = [
+    'Angular',
+    'RxJS',
+    'TypeScript',
+    'Angular Material',
+    'Bootstrap',
+    'Sass',
+    'NgRx',
+    'Jest',
+    'Cypress',
+    'Playwright',
+    'Java',
+    'Spring Boot',
+  ];
+
+  vanzoliniStack = ['HTML', 'CSS', 'JavaScript', 'PHP', 'MySQL'];
+}
