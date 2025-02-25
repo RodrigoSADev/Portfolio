e8f271fea86efd75da7b666c381ac734d8cb1bb9
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-25 12:59:32 -0300

    feat: integrate Contact component into main app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, HomeComponent, AboutComponent, ProjectsComponent],
+  imports: [
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
