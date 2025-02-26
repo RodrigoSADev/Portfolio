ffa539439df624c13dc59b5da9119f38a0edb703
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-26 17:42:49 -0300

    feat: add translations to experiences section

--- src/app/components/experiences/experiences.component.ts
+++ src/app/components/experiences/experiences.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-experiences',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './experiences.component.html',
   styleUrl: './experiences.component.scss',
 })
