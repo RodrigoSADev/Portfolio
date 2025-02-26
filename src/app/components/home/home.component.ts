8ba5affe87a35d64682cc7af8790f90129a6666b
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-26 17:32:09 -0300

    feat: add translations to home and PDF viewer components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { PdfViewerComponent } from '../../features/pdf-viewer/pdf-viewer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [PdfViewerComponent],
+  imports: [PdfViewerComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
--- src/app/features/pdf-viewer/pdf-viewer.component.ts
+++ src/app/features/pdf-viewer/pdf-viewer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
 import { Dialog } from 'primeng/dialog';
 
 @Component({
   selector: 'app-pdf-viewer',
-  imports: [PdfViewerModule, Dialog],
+  imports: [PdfViewerModule, Dialog, TranslateModule],
   templateUrl: './pdf-viewer.component.html',
   styleUrl: './pdf-viewer.component.scss',
 })
