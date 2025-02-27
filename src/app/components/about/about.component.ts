12d37d204aeb001ac5db001defb60fb725385ff2
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-27 17:33:12 -0300

    feat: add scroll animation to about section for dynamic entrance effects

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ScrollAnimationDirective } from '../../directives/scroll-animation.directive';
 import { EducationComponent } from '../education/education.component';
 import { ExperiencesComponent } from '../experiences/experiences.component';
 
 @Component({
   selector: 'app-about',
-  imports: [ExperiencesComponent, EducationComponent, TranslateModule],
+  imports: [
+    ExperiencesComponent,
+    EducationComponent,
+    TranslateModule,
+    ScrollAnimationDirective,
+  ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss',
 })
