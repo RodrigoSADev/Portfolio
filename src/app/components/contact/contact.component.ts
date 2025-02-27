71e1843129b76dfcea3f13c5b2e3a77bc64e9ea9
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-02-27 17:41:18 -0300

    feat: add scroll animation to contact section for enhanced entrance effects

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ScrollAnimationDirective } from '../../directives/scroll-animation.directive';
 
 @Component({
   selector: 'app-contact',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ScrollAnimationDirective],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
