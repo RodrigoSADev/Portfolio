4f6c609545e3b6c2f4f46e59445cc45cccd76261
Rodrigo Silva de Almeida <rodrigosalmeidadev@gmail.com>
2025-03-13 12:56:40 -0300

    feat: replace language selector dropdown with button for improved usability

--- src/app/features/language-selector/language-selector.component.ts
+++ src/app/features/language-selector/language-selector.component.ts
@@ -1,16 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-language-selector',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './language-selector.component.html',
   styleUrl: './language-selector.component.scss',
 })
 export class LanguageSelectorComponent {
   translateService = inject(TranslateService);
 
-  changeLanguage(lang: string) {
-    this.translateService.use(lang);
+  currentLanguage = this.translateService.currentLang || 'pt';
+
+  toggleLanguage() {
+    this.currentLanguage = this.currentLanguage === 'pt' ? 'en' : 'pt';
+    this.translateService.use(this.currentLanguage);
   }
 }
